123e956587579014d9d65acdf72f35f4d8f0abc0
Emiliano Lopez <lopezemiliano195@gmail.com>
2023-05-03 19:26:21 -0600

    Initial Commit

---
+++ IOSFirebase/IOSFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSFirebaseApp.swift
+//  IOSFirebase
+//
+//  Created by ISSC_611_2023 on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct IOSFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
